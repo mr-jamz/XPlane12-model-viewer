4f02ed4a8c6996b4a0091ca1d8e149d5c92dec16
Mr Jamz <nelson.dylan00@gmail.com>
2026-07-28 14:12:37 -0400

    Rollback to build v1.0.2

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/XPlane12-model-viewer/',
-})
+  base: "./",
+  build: {
+    target: "es2022",
+    sourcemap: true,
+  },
+});
